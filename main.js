167700722bea08326f94bd6405279c48afeb8ede
Moamen F. <302616064+MoamenF@users.noreply.github.com>
2026-09-11 18:45:39 +0300

    Limit and reset the diameter

--- main.js
+++ main.js
@@ -3,12 +3,14 @@
 var gBallDiameter = 100
 
 function onBallClick() {
-
     const elBall = document.querySelector('.ball')
-
+    
     gBallDiameter += 50
+    
+    if (gBallDiameter > 400) gBallDiameter = 100
 
     elBall.style.width = `${gBallDiameter}px`
     elBall.innerText = `${gBallDiameter}`
+    
 }
 
