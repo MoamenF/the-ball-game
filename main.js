9dcca5cecde539ba725e79cc19bda1498f46d318
Moamen F. <302616064+MoamenF@users.noreply.github.com>
2026-09-13 13:13:16 +0300

    Add maxDiameter parameter as size limit

--- main.js
+++ main.js
@@ -2,13 +2,26 @@
 
 var gBallDiameter = 100
 
-function onBallClick() {
+function onBallClick(maxDiameter) {
     const elBall = document.querySelector('.ball')
     
     var randAmount = getRandomInt(20, 60)
     gBallDiameter += randAmount
     
-    if (gBallDiameter > 400) gBallDiameter = 100
+    if (gBallDiameter > maxDiameter) gBallDiameter = 100
+
+    elBall.style.width = `${gBallDiameter}px`
+    elBall.innerText = `${gBallDiameter}`
+    elBall.style.backgroundColor = getRandomColor()
+}
+
+function onBall2Click(maxDiameter) {
+    const elBall = document.querySelector('.ball-2')
+    
+    var randAmount = getRandomInt(20, 60)
+    gBallDiameter += randAmount
+    
+    if (gBallDiameter > maxDiameter) gBallDiameter = 100
 
     elBall.style.width = `${gBallDiameter}px`
     elBall.innerText = `${gBallDiameter}`
